be33d83b61c74195fa195ae65f029f419af02d51
pngnptpt28 <s1718@matsue-ct.jp>
2018-03-27 16:16:29 +0900

    Check: DB n+1

--- Gemfile
+++ Gemfile
@@ -14,6 +14,7 @@ gem 'turbolinks',   '5.0.1'
 gem 'jbuilder',     '2.7.0'
 gem 'dotenv-rails'
 gem 'parallel'
+gem 'bullet', group: 'development'
 
 group :development, :test do
   gem 'sqlite3', '1.3.13'
--- app/controllers/rests_controller.rb
+++ app/controllers/rests_controller.rb
@@ -40,43 +40,41 @@ class RestsController < ApplicationController
       # processing time(API)
       @time_API = Time.now - @start_time_API    # debug
       
-      @rests = JSON.parse(json)["rest"]
+      @rests_json = JSON.parse(json)["rest"]
       
       @rests_id = Array.new
     
       
-      if @rests
+      if @rests_json
         @start_time_DB = Time.now               # debug
         
-        # DB create or update
-        @rests.each{ |rest|
+        # DB create or update(N+1)
+        p "---------------------------------N+1"
+        @rests_json.each{ |rest|
+          # DB create or update
           @rest = Rest.find_or_initialize_by(id: rest["id"])
           @rest.update_attributes(id: rest["id"], name: rest["name"], station: rest["access"]["station"], 
                                   walk: rest["access"]["walk"], shop_image1: rest["image_url"]["shop_image1"], 
                                   shop_image2: rest["image_url"]["shop_image2"], address: rest["address"],
                                   tel: rest["tel"], opentime: rest["opentime"])
-        }
-        
-        # Pass each ID to array
-        @rests.each{ |rest|
+          # Pass each ID to array
           @rests_id << rest["id"]
         }
-
-        # DB create or update
-        # @rest = Rest.find_or_initialize_by(@rests_id).includs(:rest)
-        # puts @rest
+        p "---------------------------------rests_id:", @rests_id
 
         
-        # # Pass each ID to array
-        # @rests.each{ |rest|
-        #   # DB create or update
-        #   @rest = Rest.find_or_initialize_by(id: rest["id"])
-        #   @rest.update_attributes(id: rest["id"], name: rest["name"], station: rest["access"]["station"], 
-        #                           walk: rest["access"]["walk"], shop_image1: rest["image_url"]["shop_image1"], 
-        #                           shop_image2: rest["image_url"]["shop_image2"], address: rest["address"],
-        #                           tel: rest["tel"], opentime: rest["opentime"])
-        #   # Pass each ID to array                        
-        #   @rests_id << rest["id"]
+        # DB create or update(fix)
+        # @select_rests = Rest.find_or_initialize_by(id:@rests_id)
+        # p "---------------------------------select_rests:", @select_rests
+        # @select_rests.each{ |select_rest|
+        #   @rests_json.each{ |rest_json|
+        #     if @select_rest["id"] == @rest_json["id"]
+        #       select_rest.update_attributes(name: rest_json["name"], station: rest_json["access"]["station"], 
+        #                                     walk: rest_json["access"]["walk"], shop_image1: rest_json["image_url"]["shop_image1"], 
+        #                                     shop_image2: rest_json["image_url"]["shop_image2"], address: rest_json["address"],
+        #                                     tel: rest_json["tel"], opentime: rest_json["opentime"])
+        #     end
+        #   }
         # }
         
         # processing time(DB)
