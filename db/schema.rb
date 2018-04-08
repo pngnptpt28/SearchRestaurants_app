c7b8f6eb652a611f3fc62826e3959e5081d73b86
pngnptpt28 <s1718@matsue-ct.jp>
2018-04-09 00:21:33 +0900

    Fix user model(add password)

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180408143928) do
+ActiveRecord::Schema.define(version: 20180408145036) do
 
   create_table "rests", id: :string, force: :cascade do |t|
     t.string "name"
@@ -30,6 +30,7 @@ ActiveRecord::Schema.define(version: 20180408143928) do
     t.string "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "password_digest"
   end
 
 end
