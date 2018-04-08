f108cf6c43839123bbfb9a3e62a2be39f7aae502
pngnptpt28 <s1718@matsue-ct.jp>
2018-04-08 23:47:54 +0900

    Make User model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180325043933) do
+ActiveRecord::Schema.define(version: 20180408143928) do
 
   create_table "rests", id: :string, force: :cascade do |t|
     t.string "name"
@@ -24,6 +24,12 @@ ActiveRecord::Schema.define(version: 20180325043933) do
     t.string "tel"
     t.string "opentime"
     t.index ["id"], name: "sqlite_autoindex_rests_1", unique: true
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
 end
